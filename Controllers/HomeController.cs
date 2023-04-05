0a92f6a02d0ad61c489e2c2ccb5adf9b6bcb500a
BuildTools <unconfigured@null.spigotmc.org>
2023-04-05 22:23:25 +0700

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,4 +28,6 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    #testt
 }
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,6 +2,7 @@ namespace dotnet_food.Models;
 
 public class ErrorViewModel
 {
+    public string test;
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
