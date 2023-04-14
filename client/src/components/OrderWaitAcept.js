e1ac1f19e1e89491e0d7ef5207561d09ff6cbb1c
DarkMerlin <DarkMerlin@LAPTOP-NKGS89P9>
2023-04-14 16:34:06 +0700

    Update OrderWaitAcept

--- client/src/components/OrderWaitAcept.js
+++ client/src/components/OrderWaitAcept.js
@@ -1,13 +1,55 @@
 import React from 'react'
 import NavBar from './NavBar'
 import Footer from './Footer'
+import '../index.css'
+import { Oval } from 'react-loader-spinner'
 
 export default function OrderWaitAcept() {
   return (
     <div>
       <NavBar />
-      <div>
-        OrderWaitAcept ควย
+      <div className='mx-auto bg-scale-300 h-screen'>
+        {/* Waiting Progress */}
+        <div className='flex-col items-center bg-teal-400'>
+          <div className='spinner-container p-2'>
+          <Oval 
+            height={200}
+            width={200}
+            color="orange"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            ariaLabel='loading-indicator'
+            secondaryColor="white"
+            strokeWidth={2}
+            strokeWidthSecondary={2}
+          />
+          </div>
+          <h1 className='text-3xl font-bold mx-auto text-orange-400 p-4 text-center'>Order Status : Waiting For Accept Order</h1>
+          <h4 className='text-2xl text-orange-400 mx-auto p-2 text-center'>Waiting for a moment...</h4>
+        </div>
+        {/* Order */}
+        <div className='p-8'></div>
+        <div class="flex flex-row px-10 py-4 bg-teal-200">
+          <div class='flex flex-none flex-row bg-sky-600 rounded w-20 h-20 items-center justify-center'><h1 className='text-3xl font-bold'>2</h1></div>
+          <div class='flex flex-1 w-64 items-center px-10'>ชื่ออาหาร</div>
+          <div class='flex flex-1 w-32 items-center justify-end mx-10'>ราคา</div>
+        </div>
+        <div className='p-4'></div>
+        <div class="flex flex-row px-10 py-4 bg-teal-200">
+          <div class='flex flex-none flex-row bg-sky-600 rounded w-20 h-20 items-center justify-center'><h1 className='text-3xl font-bold'>1</h1></div>
+          <div class='flex flex-1 w-64 items-center px-10'>ชื่ออาหาร</div>
+          <div class='flex flex-1 w-32 items-center justify-end mx-10'>ราคา</div>
+        </div>
+        <div className='p-4'></div>
+        <div class="flex flex-row px-10 py-4 bg-teal-200">
+          <div class='flex flex-none flex-row bg-sky-600 rounded w-20 h-20 items-center justify-center'><h1 className='text-3xl font-bold'>1</h1></div>
+          <div class='flex flex-1 w-64 items-center px-10'>ชื่ออาหาร</div>
+          <div class='flex flex-1 w-32 items-center justify-end mx-10'>ราคา</div>
+        </div>
+        <div className='flex justify-center p-8'>
+          <a href='#' class='p-3 px-20 pt-2 bg-teal-400 rounded-full baseline hover:bg-teal-300'>Cancle Order</a>
+        </div>
       </div>
       <Footer />
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+    },
   },
   variants: {
     extend: {},
