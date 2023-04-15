4997698508dca3d0f8819faa6862fd39ac01905c
Parames Angkoolpakdeekul <kaooat.prm@gmail.com>
2023-04-15 21:59:53 +0700

    Update Address text-area , Phone pattern

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -22,11 +22,11 @@ export default function Register() {
             </div>
             <div>
               <label className="block mb-1 font-bold text-gray-500">Address <ImAddressBook className="inline-block mr-2 mb-1" /></label>
-              <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-300" placeholder="Enter your address" />
+              <textarea type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-300" placeholder="Enter your address"></textarea>
             </div>
             <div>
               <label className="block mb-1 font-bold text-gray-500">Phone Number <HiPhone className="inline-block mr-2 mb-1" /></label>
-              <input type="text" pattern="[0-9]*" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-300" placeholder="Enter your phone number" />
+              <input type="text" pattern="[0-9-]*" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-300" placeholder="Enter your phone number" />
             </div>
             <button type="submit" className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">Register</button>
           </form>
