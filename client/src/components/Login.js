87d15237b17bfaf8834c420d1de9b2892d94c73c
Parames Angkoolpakdeekul <kaooat.prm@gmail.com>
2023-04-15 20:50:11 +0700

    Update Login and Register

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import NavBar from './NavBar'
 import Footer from './Footer'
+import { HiOutlineUser, HiOutlineLockClosed } from 'react-icons/hi';
 
 export default function Login() {
   return (
     <div>
-        <NavBar />
-          this is login page
-        <Footer />
+      <NavBar />
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+          <h2 className="text-2xl font-bold mb-4">Log In</h2>
+          <form className="space-y-4">
+            <div>
+              <label className="block mb-1 font-bold text-gray-500">Username <HiOutlineUser className="inline-block mr-2 mb-1" /></label>
+              <input type="username" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-300" placeholder="Enter your username" />
+            </div>
+            <div>
+              <label className="block mb-1 font-bold text-gray-500">Password <HiOutlineLockClosed className="inline-block mr-2 mb-1" /></label>
+              <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-300" placeholder="Enter your password" />
+            </div>
+            <button type="submit" className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">Log In</button>
+          </form>
+          <div className="mt-4 flex items-center justify-center">
+            <span className="text-gray-500 mr-2">Don't have an account?</span>
+            <a href="/register" className="text-blue-500 hover:underline">
+              Register
+            </a>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
