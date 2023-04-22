88d10448682eec4e1bd55761f634ac1c911f7600
BuildTools <unconfigured@null.spigotmc.org>
2023-04-22 21:25:50 +0700

    Add FoodItems

---
+++ Migrations/ShopItemsData/20230422142455_AddFood.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_food.Migrations.ShopItemsData
+{
+    /// <inheritdoc />
+    public partial class AddFood : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
